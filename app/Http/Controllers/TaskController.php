adcc047ffb22f1898693edb6a6cf90b1b95e04ca
João Patrocínio <joaoafonsopatrocinio@gmail.com>
2024-12-20 00:48:02 +0000

    Added task controller and model

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return response()->json([
+            'message' => 'Task list complete.',
+            'payload' => $tasks
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::find($id);
+
+        if (empty($task)) {
+            return response()->json([
+                'message' => 'Task not found.',
+                'payload' => null
+            ], 404);
+        }
+
+        return response()->json([
+            'message' => 'List complete.',
+            'payload' => $task
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
